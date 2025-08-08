a4ce0457dea7c7d403aacf285932a4771747ec48
gsnurr3 <gsnurr3@hotmail.com>
2025-08-08 12:44:37 -0400

    Add gitignore

--- tests/obj/Debug/net8.0/RESTfulAPI.Tests.AssemblyInfo.cs
+++ tests/obj/Debug/net8.0/RESTfulAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RESTfulAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5fd2bb1ffe2b1e3c39f1c68e0dccae226457db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+611b98a8d17286345924a88f501ed30dd378030d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RESTfulAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RESTfulAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
